0892be7807ee714f7b8d7262231f51eb2366e71c
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-04-15 00:17:04 +0900

    Revert "make: Show inclusion of makefiles"
    This reverts commit 5464fd6a04fe246d8713c901bd7cdbdc7af41b51.

--- core/main.mk
+++ core/main.mk
@@ -518,11 +518,7 @@ else # ONE_SHOT_MAKEFILE
 subdir_makefiles := \
 	$(shell build/tools/findleaves.py --prune=out --prune=.repo --prune=.git $(subdirs) Android.mk)
 
-$(foreach subdir_makefile, $(subdir_makefiles), \
-$(info Including $(subdir_makefile)) \
-$(eval include $(subdir_makefile)) \
-)
-subdir_makefile :=
+include $(subdir_makefiles)
 
 endif # ONE_SHOT_MAKEFILE
 
